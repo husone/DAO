4b123c63d2c1d28ef0e490b665f6503d5495c4e4
husone <sonhvnde160056@fpt.edu.vn>
2022-09-20 10:34:23 +0700

    inital commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    georli: {
+      url: process.env.RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+};
